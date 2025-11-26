db77281001293a2cf0b15c960d7c190098acae81
Dest <dest-supp@proton.me>
2025-11-26 23:02:21 +0200

    Made scanner for .desktop files and parser for .desktop files

--- src/core/apps/parser.rs
+++ src/core/apps/parser.rs
@@ -1,14 +1,63 @@
-use std::{fs, path::PathBuf};
+use std::{ffi::OsStr, fs, path::PathBuf};
 
-// use freedesktop_entry_parser::parse_entry;
+use freedesktop_file_parser::EntryType;
 
-pub fn parse_data() {
-    println!("Parsing data...");
-    let mut desktops_paths: Vec<PathBuf> = Vec::new();
-    if let Ok(entries) = fs::read_dir(PathBuf::from("/usr/share/applications")) // Verify if read_dir don't get any errors
-    {
-        desktops_paths.extend(entries.flatten().map(|e| e.path())); // Push in vector all files that is in system_dir
+use crate::core::apps::{model::AppList, scanner::scan_desktop_files};
+
+pub fn parse_data() -> Vec<AppList> {
+    let desktops_paths: Vec<PathBuf> = scan_desktop_files();
+    let mut apps_info: Vec<AppList> = Vec::new();
+    for entry in &desktops_paths {
+
+        if entry.extension() != Some(OsStr::new("desktop")) || entry.is_dir()
+        {
+            continue;
+        }
+        // Move to the next if current is not a .desktop file
+        
+        let content = match fs::read_to_string(entry) {
+            Ok(content) => content,
+            Err(_) => continue,
+        };
+        // Get content from .desktop file
+        let desktop_file = match freedesktop_file_parser::parse(&content) {
+            Ok(file) => file,
+            Err(_) => continue,
+        };
+        // Get all attr from desktop file
+
+        if desktop_file.entry.hidden == Some(true) || desktop_file.entry.no_display == Some(true) {
+            continue;
+        }
+        // If app in .desktop file is hidden, skip app
+
+        let name = desktop_file.entry.name.default;
+        // Get name of the app in .desktop file
+
+        let description = desktop_file.entry.comment;
+        // Get description of the app in .desktop file
+
+        let icon = desktop_file.entry.icon;
+        // Get icon of the app in .desktop file
+
+        if let EntryType::Application(app) = &desktop_file.entry.entry_type {
+            let exec = match &app.exec {
+                Some(exec) => exec.clone(),
+                None => continue,
+            };
+            // Get exec command of the app in .desktop file
+
+            apps_info.push(
+                AppList {
+                    name: name,
+                    description: description.unwrap_or_default().default,
+                    exec: exec.clone(),
+                    icon_path: icon.unwrap_or_default().content.into(),
+                    type_file: "Application".to_string()
+                }
+            );
+            // Push app in list of apps
+        }
     }
-    println!("Desktops: {:?}", desktops_paths);
+    apps_info
 }
-
